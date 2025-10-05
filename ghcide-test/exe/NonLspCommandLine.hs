5695cf118d51cfba60fd9099a88c2d15504ef9bc
soulomoon <patrickwalesss@gmail.com>
2025-10-05 15:20:04 +0800

    refactor: enhance ghcide executable location handling

--- ghcide-test/exe/NonLspCommandLine.hs
+++ ghcide-test/exe/NonLspCommandLine.hs
@@ -1,20 +1,23 @@
 
 module NonLspCommandLine (tests) where
 
+import           Config                   (testDataDir)
+import           Control.Exception        (throwIO)
 import           Control.Monad            ((>=>))
 import           Data.Foldable            (for_)
 import           Development.Shake        (getDirectoryFilesIO)
-import           System.Directory         (copyFile, createDirectoryIfMissing)
+import           System.Directory         (copyFile, createDirectoryIfMissing,
+                                           makeAbsolute)
 import           System.Directory.Extra   (canonicalizePath)
 import           System.Environment.Blank (setEnv)
 import           System.Exit              (ExitCode (ExitSuccess))
 import           System.FilePath          (takeDirectory, (</>))
 import qualified System.IO.Extra
+import           System.Process           (readProcess)
 import           System.Process.Extra     (CreateProcess (cwd), proc,
                                            readCreateProcessWithExitCode)
 import           Test.Tasty
 import           Test.Tasty.HUnit
-import           Config                   (testDataDir)
 
 
 -- A test to ensure that the command line ghcide workflow stays working
@@ -32,8 +35,16 @@ tests = testGroup "ghcide command line"
         ec @?= ExitSuccess
   ]
 
+
+
 locateGhcideExecutable :: IO FilePath
-locateGhcideExecutable = pure "ghcide"
+locateGhcideExecutable = do
+  -- Run the find command to locate the ghcide executable
+  out <- readProcess "find" ["dist-newstyle", "-type", "f", "-name", "ghcide"] ""
+  case lines out of
+    (path:_) -> makeAbsolute path
+    []       -> throwIO $ userError "ghcide executable not found in dist-newstyle"
+
 
 -- | Version of 'System.IO.Extra.withTempDir' that canonicalizes the path
 -- Which we need to do on macOS since the $TMPDIR can be in @/private/var@ or
