80bee0cf1c4ace070703accb6ae962440c07e1d1
soulomoon <patrickwalesss@gmail.com>
2025-11-07 01:45:37 +0800

    revert getCacheDirsDefault

--- ghcide/session-loader/Development/IDE/Session/Ghc.hs
+++ ghcide/session-loader/Development/IDE/Session/Ghc.hs
@@ -422,9 +422,9 @@ getCacheDirsDefault root prefix opts = do
     where
         -- Create a unique folder per set of different GHC options, assuming that each different set of
         -- GHC options will create incompatible interface files.
-        -- opts_hash = B.unpack $ B16.encode $ H.finalize $ H.updates H.init (map B.pack opts)
+        opts_hash = B.unpack $ B16.encode $ H.finalize $ H.updates H.init (map B.pack opts)
         -- opts_hash = "fixed"
-        opts_hash = B.unpack $ B16.encode $ H.finalize $ H.updates H.init (map B.pack [root])
+        -- opts_hash = B.unpack $ B16.encode $ H.finalize $ H.updates H.init (map B.pack [root])
 
 getOptionHash :: [String] -> String
 getOptionHash opts = B.unpack $ B16.encode $ H.finalize $ H.updates H.init (map B.pack opts)
