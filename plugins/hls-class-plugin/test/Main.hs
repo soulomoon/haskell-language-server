003dd54d58d4471b0b3bca94b3d77de71a2cb9bd
soulomoon <patrickwalesss@gmail.com>
2025-11-07 21:11:41 +0800

    use waitForBuildQueue in hls-class-plugin

--- plugins/hls-class-plugin/test/Main.hs
+++ plugins/hls-class-plugin/test/Main.hs
@@ -161,7 +161,8 @@ goldenCodeLens title path idx =
 goldenWithClass ::TestName -> FilePath -> FilePath -> ([CodeAction] -> Session CodeAction) -> TestTree
 goldenWithClass title path desc findAction =
   goldenWithHaskellDoc def classPlugin title testDataDir path (desc <.> "expected") "hs" $ \doc -> do
-    _ <- waitForDiagnosticsFrom doc
+    -- _ <- waitForDiagnosticsFrom doc
+    waitForBuildQueue
     actions <- concatMap (^.. _CACodeAction) <$> getAllCodeActions doc
     action <- findAction actions
     executeCodeAction action
