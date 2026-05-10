8cfc681e1f7f2e16f920b6ab16203023f6b3dc34
soulomoon <patrickwalesss@gmail.com>
2026-05-11 04:46:42 +0800

    Match runtime engine dirty-key behavior

--- ghcide/src/Development/IDE/Core/Rules.hs
+++ ghcide/src/Development/IDE/Core/Rules.hs
@@ -615,9 +615,12 @@ typeCheckRule :: Recorder (WithPriority Log) -> Rules ()
 typeCheckRule recorder = define (cmapWithPrio LogShake recorder) $ \TypeCheck file -> do
     pm <- use_ GetParsedModule file
     hsc  <- hscEnv <$> use_ GhcSessionDeps file
+    foi <- use_ IsFileOfInterest file
     -- We should only call the typecheck rule for files of interest.
     -- Keeping typechecked modules in memory for other files is
     -- very expensive.
+    when (foi == NotFOI) $
+      logWith recorder Logger.Warning $ LogTypecheckedFOI file
     typeCheckRuleDefinition hsc pm file
 
 knownFilesRule :: Recorder (WithPriority Log) -> Rules ()
